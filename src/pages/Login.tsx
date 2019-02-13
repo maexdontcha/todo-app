69da050e0675b8b365ec34c010570f9923b793bb
Max Hänsel <mh@maxhaensel.de>
2019-02-13 12:05:15 +0100

    save routs

---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+class Login extends Component {
+  render() {
+    return <React.Fragment>Login</React.Fragment>
+  }
+}
+
+export default Login
